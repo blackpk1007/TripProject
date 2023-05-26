3534544d2817da53c0646c696ada1abcf8fd0c48
Jeon Young Su <blackpk1007@gmail.com>
2023-05-26 17:18:10 +0900

    usercontroller add
    usercontroller add

---
+++ src/main/java/com/trip/project/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.trip.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	
+	// 사용자 마이페이지 메인
+	@RequestMapping
+	public String userMain() {
+		
+		return "usermain";
+	}
+	
+	// 사용자 회원 정보 수정 페이지
+	@RequestMapping("userupdateform")
+	public String userUpdateForm() {
+		
+		return "userupdateform";
+	}
+	
+	// 사용자 회원 정보 수정
+	@RequestMapping("userupdate")
+	public String userupdate() {
+		
+		return "userupdate";
+	}
+	
+	// 사용자 회원 탈퇴
+	@RequestMapping("userdelete")
+	public String userDelete() {
+		
+		return "userdelete";
+	}
+	
+	
+	
+	
+	
+}
