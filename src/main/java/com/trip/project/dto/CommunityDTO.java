92a294fafc27c49b49599a7436b1b9ba629859bf
Jeon Young Su <blackpk1007@gmail.com>
2023-05-26 15:37:27 +0900

    Dto update
    CommunityDto, LoginDto add

--- src/main/java/com/trip/project/dto/CommunityDTO.java
+++ src/main/java/com/trip/project/dto/CommunityDTO.java
@@ -1,5 +1,33 @@
 package com.trip.project.dto;
 
-public class CommunityDTO {
+import java.util.Date;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CommunityDTO {
+	
+	private int communityNumber;
+	private String communityTitle;
+	private String communityContent;
+	private Date communityDate;
+	private String communityCategory;
+	
+	public CommunityDTO() {
+		super();
+	}
+	
+	public CommunityDTO(int communityNumber, String communityTitle, String communityContent, Date communityDate,
+			String communityCategory) {
+		super();
+		this.communityNumber = communityNumber;
+		this.communityTitle = communityTitle;
+		this.communityContent = communityContent;
+		this.communityDate = communityDate;
+		this.communityCategory = communityCategory;
+	}
+	
+	
 }
--- src/main/java/com/trip/project/dto/LoginDTO.java
+++ src/main/java/com/trip/project/dto/LoginDTO.java
@@ -1,5 +1,36 @@
 package com.trip.project.dto;
 
-public class LoginDTO {
+import java.util.Date;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginDTO {
+	
+	private int userNumber;
+	private String userName;
+	private String userID;
+	private String userPW;
+	private String userEmail;
+	private String userGender;
+	private Date userBirth;
+	
+	public LoginDTO() {
+		super();
+	}
+	
+	public LoginDTO(int userNumber, String userName, String userID, String userPW, String userEmail, String userGender,
+			Date userBirth) {
+		super();
+		this.userNumber = userNumber;
+		this.userName = userName;
+		this.userID = userID;
+		this.userPW = userPW;
+		this.userEmail = userEmail;
+		this.userGender = userGender;
+		this.userBirth = userBirth;
+	}
+	
 }
