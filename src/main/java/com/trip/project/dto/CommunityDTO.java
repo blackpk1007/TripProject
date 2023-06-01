650c55eae9864833e1628a1927aec0452e3c0818
Jeon Young Su <blackpk1007@gmail.com>
2023-06-01 17:41:38 +0900

    DTO lombok add, sql add
    DTO lombok 설정, sql table 생성 쿼리 파일 추가

--- src/main/java/com/trip/project/dto/CommunityDTO.java
+++ src/main/java/com/trip/project/dto/CommunityDTO.java
@@ -2,11 +2,9 @@ package com.trip.project.dto;
 
 import java.util.Date;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 public class CommunityDTO {
 	
 	private int communityNumber;
