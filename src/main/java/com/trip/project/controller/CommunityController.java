e2e0e92e4206aaf5945dbc8eebf6f3a519f9f148
fiow4957 <fiow4957@naver.com>
2023-06-02 10:18:47 +0900

    커뮤니티 기능 및 html
    집에서 했던 커뮤니티 기능(컨트롤러, 서비스, mapper), html 추가하였습니다

--- src/main/java/com/trip/project/controller/CommunityController.java
+++ src/main/java/com/trip/project/controller/CommunityController.java
@@ -1,65 +1,87 @@
 package com.trip.project.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.trip.project.dto.CommunityDTO;
 import com.trip.project.service.CommunityService;
 
 @Controller
 @RequestMapping("/community")
 public class CommunityController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(CommunityController.class);
+	
 	@Autowired
 	private CommunityService cService;
 	
 	// 커뮤니티 메인 페이지
-	@RequestMapping
-	public String cummunityMain() {
-		
+	@RequestMapping("/communitymain")
+	public String cummunityMain(Model model) {
+		logger.info("COMMUNITY MAIN");
+		model.addAttribute("dto", cService.selectCommunity());
 		return "communitymain";
 	}
 	
 	// 커뮤니티 상세 페이지
 	@RequestMapping("/communitydetail")
-	public String communityDetail() {
-		
+	public String communityDetail(Model model, int communityNumber) {
+		logger.info("COMMUNITY DETAIL");
+	    model.addAttribute("dto", cService.selectOne(communityNumber));
 		return "communitydetail";
 	}
 	
 	// 커뮤니티 글쓰기 페이지
 	@RequestMapping("/communitywriteform")
 	public String communityWriteForm() {
-		
+		logger.info("COMMUNITY WRITE FORM");
 		return "communitywriteform";
 	}
 	
 	// 커뮤니티 글쓰기
 	@RequestMapping("/communitywrite")
-	public String communityWrite() {
+	public String communityWrite(CommunityDTO dto) {
+		logger.info("COMMUNITY WRITE");
+		if(cService.insert(dto)>0) {
+			return "redirect:/communitymain";
+		}else {
+			return "redirect:/communitywriteform";
+		}
 		
-		return "communitywrite";
 	}
 	
 	// 커뮤니티 수정 페이지
 	@RequestMapping("/communityupdateform")
-	public String communityUpdateForm() {
-		
+	public String communityUpdateForm(Model model, int communityNumber) {
+		logger.info("UPDATEFORM COMMUNITY");
+		model.addAttribute("dto", cService.selectOne(communityNumber));
 		return "communityupdateform";
 	}
 	
 	// 커뮤니티 수정
 	@RequestMapping("/communityupdate")
-	public String communityUpdate(){
-		
-		return "communityupdate";
+	public String communityUpdate(CommunityDTO dto){
+		logger.info("UPDATE COMMUNITY");
+		if(cService.update(dto)>0) {
+			return "redirect:/communitymain";
+		}else {
+			return "redirect:/communityupdateform";
+		}
 	}
 	
 	// 커뮤니티 삭제
 	@RequestMapping("/communitydelete")
-	public String communityDelete() {
-		
-		return "communitydelete";
+	public String communityDelete(int communityNumber) {
+		logger.info("DELETE COMMUNITY");
+		if(cService.delete(communityNumber)>0) {
+			return "redirect:/community";
+		}else {
+			return "redirect:/detail?communityNumber"+communityNumber;
+		}
 	}
 }
 
