3e11874cb3ff3f9065a510cd7b5e716cffd2230b
JIYEON YU <cp9835@naver.com>
2023-06-16 23:33:33 +0900

    menu&ocr
    마커를 클릭했을때 왼쪽 목록이 사라지고 장소 상세보기가 뜨고 장소상세보기 닫으면 원래대로 목록이 뜨도록 구현
    
    장소상세보기에서 추천하기를 누르면 영수증을 업로드할 수 있는 화면이 뜸
    영수증을 업로드하고 등록버튼을 누르면 텍스트를 추출해서 설정해놓은 디비의 장소 데이터와 영수증의 장소가 일치하는지 비교하는것 구현

---
+++ src/main/java/com/trip/project/controller/ReivewController.java
@@ -0,0 +1,53 @@
+package com.trip.project.controller;
+
+import java.io.IOException;
+
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.trip.project.service.CommunityService;
+import com.trip.project.service.Ocr;
+import com.trip.project.service.OcrTest;
+
+@Controller
+@RequestMapping("/review")
+public class ReivewController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(CommunityController.class);
+	
+	@Autowired
+	private CommunityService cService;
+	
+	@Autowired
+	ResourceLoader resourceLoader;
+	
+	@Autowired
+	private OcrTest OcrTest;
+	@RequestMapping
+	public String reviewmain() {
+		
+		return "mypagetest";
+	}
+	
+	@PostMapping("/reveiwwrite")
+    public String review(@RequestParam("attachFile") MultipartFile file, Model model) throws IOException {
+        Ocr ocr = new Ocr();
+        String res = ocr.ocr(file);
+        JSONObject obj = new JSONObject(res);
+        model.addAttribute("res", obj);
+        model.addAttribute("num", OcrTest.selectOne());
+        System.out.println("추출된 텍스트:");
+        System.out.println(obj.toString());
+
+        return "ocr";
+    }
+}
