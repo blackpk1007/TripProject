eb5b11c016eb96a024f84de522468584c5cef44a
감자 <68111122+Bryan051@users.noreply.github.com>
2023-07-05 01:31:59 +0900

    일정 삭제

--- src/main/java/com/trip/project/controller/LoginController.java
+++ src/main/java/com/trip/project/controller/LoginController.java
@@ -7,12 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.trip.project.dto.LoginDTO;
+import com.trip.project.dto.PlanDTO;
 import com.trip.project.service.CommunityService;
 import com.trip.project.service.LoginService;
 import com.trip.project.service.PlanService;
@@ -218,6 +220,15 @@ public class LoginController {
 		return "userupdateform";
 	}
 
+	//plandelete 삭제
+	@ResponseBody
+	@RequestMapping("/plandelete")
+	public String planDelete(HttpSession session, PlanDTO dto) {
+		int res = pservice.planDelete(dto);
+		System.out.println(res);
+		return dto.getUserID()+"님의"+dto.getPlanName()+"이 삭제되었습니다.";
+	}
+	
 	// 사용자 회원 정보 수정
 	@ResponseBody
 	@RequestMapping("/userupdate")
